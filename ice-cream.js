d09790d387c5c7762492031f2672eaaf768da6f1
nicholasrwx <digitalexistence18@gmail.com>
2020-11-13 10:28:21 +0000

    using different array properties

---
+++ ice-cream.js
@@ -0,0 +1,22 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+
+//add rootbeer
+
+iceCreamFlavours.splice(5, 0, "root beer");
+console.log(iceCreamFlavours);
+
+//get first flavor
+
+console.log(iceCreamFlavours[0]);
+
+//get last flavor
+
+console.log(iceCreamFlavours[5]);
+
+// total number
+
+console.log(iceCreamFlavours.length);
+
+
